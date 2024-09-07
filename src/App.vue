8cf9fa8db5a3f1f6c74e970b3d39b9d3be8fa142
JeongHyunBaek <bjhbhm@gmail.com>
2024-09-07 12:38:39 +0900

    init commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <Header />
+    <router-view></router-view>
+    <Footer />
+  </div>
+</template>
+
+<script setup>
+import Footer from './components/Footer.vue';
+import Header from './components/Header.vue';
+</script>
+
+<style lang="scss" scoped></style>
