b71096142732605968a24de3a63b3af86b4d3ae3
heebp <skdnfl4343@gmail.com>
2024-10-01 14:11:15 +0900

    refactor: pinia로 종목 아이디 상태 관리

---
+++ src/pages/StockPage.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h2>Post ID: {{ stockId }}</h2>
+  </div>
+</template>
+
+<script setup>
+import { onMounted } from "vue";
+import { useHeaderStore } from "@/stores/headerStore";
+const props = defineProps({
+  stockId: {
+    type: String,
+    required: true,
+  },
+});
+
+// store 가져오기
+const headerStore = useHeaderStore();
+
+// 타이틀 변경 함수
+const changeTitle = (stockId) => {
+  headerStore.setTitle(stockId);
+};
+onMounted(() => {
+  changeTitle(props.stockId);
+});
+</script>
