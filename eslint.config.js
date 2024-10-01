523718acaeb05e7392e720692949cde9e329f4e0
heebp <skdnfl4343@gmail.com>
2024-10-01 14:06:41 +0900

    chore: Vue 3 Composition api eslint 설정

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,6 @@ import prettierPlugin from "eslint-plugin-prettier";
 
 export default [
   ...pluginVue.configs["flat/recommended"],
-  // prettier,
   eslintConfigPrettier,
   // add more generic rulesets here, such as:
   // js.configs.recommended,
@@ -17,21 +16,20 @@ export default [
       // typescriptEslint,
       // But in this example we give it another name.
       // It might be tempting to use something shorter like “ts”:
-      // "prettier"
       prettier: prettierPlugin,
     },
     rules: {
-      // override/add rules settings here, such as:
-      // 'vue/no-unused-vars': 'error'
       "prettier/prettier": [
         "error",
         {
           endOfLine: "auto", // 줄 끝 스타일을 자동으로 설정 (CRLF/LF 혼용 허용)
         },
       ],
+      "vue/component-api-style": [
+        "error",
+        ["script-setup", "composition"], // "script-setup", "composition", "composition-vue2", or "options"
+      ],
       "vue/no-unused-vars": "error", // Vue.js에서 사용되지 않는 변수 에러 처리
-      // ...prettierPlugin.configs.recommended.rules,
-      // 'prettier': 'error',   // Prettier 규칙 위반 시 에러 처리
     },
   },
 ];
