a47339094724780071358688bd7273b3ef92afdc
heebp <skdnfl4343@gmail.com>
2024-10-04 16:57:32 +0900

    chore: highchart 라이브러리 설치

--- src/main.js
+++ src/main.js
@@ -4,10 +4,12 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
+import HighchartsVue from "highcharts-vue";
 
 const app = createApp(App);
 const pinia = createPinia();
 
 app.use(pinia);
 app.use(router);
+app.use(HighchartsVue);
 app.mount("#app");
