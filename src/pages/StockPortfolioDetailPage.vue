5a0977772bc552428c32e4f2c9558ad4917ad27d
heebp <skdnfl4343@gmail.com>
2024-10-04 17:06:54 +0900

    feat: 종목 주문 내역 화면 레이아웃 구현

---
+++ src/pages/StockPortfolioDetailPage.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>StockPortfolioDetail{{ stockId }}</div>
+</template>
+
+<script setup>
+const props = defineProps({
+  stockId: {
+    type: String,
+    required: true,
+  },
+});
+
+async function fetchStockPortfolioDetailData() {
+  const response = await axios.get(
+    `${import.meta.env.VITE_API_URL}/portfolio/${props.stockId}/order`
+  );
+  return response.data;
+}
+</script>
+
+<style scoped>
+div {
+  height: 48px;
+}
+</style>
