9f6c9cca0890efff6574cfad112e828195ddd990
JeongHyunBaek <bjhbhm@gmail.com>
2024-10-05 19:59:30 +0900

    feat : HomePage, DailyReportPAge 추가

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,73 +1,56 @@
 <template>
-  <div>
-    Lorem ipsum odor amet, consectetuer adipiscing elit. Bibendum augue massa
-    viverra no n montes. Luctus inceptos curabitur integer habitant in senectus.
-    Hac justo ultricies lacinia auctor vivamus urna. Facilisis fringilla ad
-    mauris; auctor praesent lobortis fusce magna curabitur. Nibh tempor sed
-    viverra inceptos fames leo a platea platea. Dui maecenas lacinia porttitor
-    posuere dis, eleifend taciti. Maximus feugiat augue venenatis placerat in
-    aptent adipiscing. Odio ullamcorper inceptos bibendum; egestas dis aptent.
-    Dapibus massa aliquet ligula taciti nostra. Posuere ligula massa rutrum
-    senectus habitasse maximus integer odio. Eu fusce morbi ad, nisl ad
-    vestibulum nec consequat. Bibendum facilisi sagittis velit finibus aenean.
-    Praesent senectus pulvinar vestibulum augue finibus convallis ultrices
-    suscipit. Efficitur dis proin ante dui litora nec nostra. Cursus nunc sapien
-    laoreet faucibus tempus eu sit. Sodales commodo senectus eget; libero
-    praesent malesuada dis inceptos. Facilisis massa risus velit blandit,
-    commodo diam mi. Senectus id nulla mus venenatis felis parturient. Habitant
-    molestie sociosqu molestie placerat enim ultricies. Habitant odio mus eget
-    class, elementum suscipit fermentum in parturient. Varius quisque gravida
-    egestas scelerisque nisl aptent. Quisque blandit duis himenaeos morbi turpis
-    dictumst vulputate pretium? Curabitur auctor ac finibus parturient
-    sollicitudin. Est elit adipiscing blandit morbi, lacinia risus. Dolor turpis
-    nascetur sociosqu fermentum parturient in lobortis. Et suscipit sollicitudin
-    morbi massa lobortis congue tristique velit. Mollis nisi facilisi hendrerit
-    ante risus mattis. Fringilla varius feugiat faucibus magnis ligula nascetur
-    dictum. Nullam iaculis purus aliquam nullam facilisis nunc class. Integer
-    habitant commodo maximus pulvinar aliquam duis. Non sagittis ultrices etiam
-    dapibus ac. Aliquet mauris proin felis sagittis quam pulvinar per. Lectus
-    tristique porttitor at interdum habitasse efficitur. Etiam ad curae nascetur
-    fusce proin ut sagittis fusce ridiculus. Lacinia pellentesque eleifend
-    senectus montes, enim vestibulum. Ipsum nam ut eget tortor non, posuere
-    malesuada class dapibus. Metus sociosqu rutrum aliquet hac convallis. Ligula
-    curae tempus faucibus dictum purus libero. Torquent neque tempus egestas
-    arcu mi habitasse elementum proin. Vestibulum porttitor facilisis ante proin
-    habitasse. Lorem ipsum odor amet, consectetuer adipiscing elit. Bibendum
-    augue massa viverra non montes. Luctus inceptos curabitur integer habitant
-    in senectus. Hac justo ultricies lacinia auctor vivamus urna. Facilisis
-    fringilla ad mauris; auctor praesent lobortis fusce magna curabitur. Nibh
-    tempor sed viverra inceptos fames leo a platea platea. Dui maecenas lacinia
-    porttitor posuere dis, eleifend taciti. Maximus feugiat augue venenatis
-    placerat in aptent adipiscing. Odio ullamcorper inceptos bibendum; egestas
-    dis aptent. Dapibus massa aliquet ligula taciti nostra. Posuere ligula massa
-    rutrum senectus habitasse maximus integer odio. Eu fusce morbi ad, nisl ad
-    vestibulum nec consequat. Bibendum facilisi sagittis velit finibus aenean.
-    Praesent senectus pulvinar vestibulum augue finibus convallis ultrices
-    suscipit. Efficitur dis proin ante dui litora nec nostra. Cursus nunc sapien
-    laoreet faucibus tempus eu sit. Sodales commodo senectus eget; libero
-    praesent malesuada dis inceptos. Facilisis massa risus velit blandit,
-    commodo diam mi. Senectus id nulla mus venenatis felis parturient. Habitant
-    molestie sociosqu molestie placerat enim ultricies. Habitant odio mus eget
-    class, elementum suscipit fermentum in parturient. Varius quisque gravida
-    egestas scelerisque nisl aptent. Quisque blandit duis himenaeos morbi turpis
-    dictumst vulputate pretium? Curabitur auctor ac finibus parturient
-    sollicitudin. Est elit adipiscing blandit morbi, lacinia risus. Dolor turpis
-    nascetur sociosqu fermentum parturient in lobortis. Et suscipit sollicitudin
-    morbi massa lobortis congue tristique velit. Mollis nisi facilisi hendrerit
-    ante risus mattis. Fringilla varius feugiat faucibus magnis ligula nascetur
-    dictum. Nullam iaculis purus aliquam nullam facilisis nunc class. Integer
-    habitant commodo maximus pulvinar aliquam duis. Non sagittis ultrices etiam
-    dapibus ac. Aliquet mauris proin felis sagittis quam pulvinar per. Lectus
-    tristique porttitor at interdum habitasse efficitur. Etiam ad curae nascetur
-    fusce proin ut sagittis fusce ridiculus. Lacinia pellentesque eleifend
-    senectus montes, enim vestibulum. Ipsum nam ut eget tortor non, posuere
-    malesuada class dapibus. Metus sociosqu rutrum aliquet hac convallis. Ligula
-    curae tempus faucibus dictum purus libero. Torquent neque tempus egestas
-    arcu mi habitasse elementum proin. Vestibulum porttitor facilisis ante proin
-    habitasse.
+  <div class="home">
+    <header class="header">
+      <h1>PGMA(로고)</h1>
+      <p class="report-time">다음 리포트까지 23시간 남았어요!</p>
+    </header>
+
+    <section class="summary">
+      <HomeDailyTrendPage />
+    </section>
+
+    <section class="portfolio">
+      <HomePortfolioPage />
+    </section>
+
+    <section class="recommendations">
+      <HomeRecommendStockPage />
+    </section>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import HomeDailyTrendPage from "./HomeDailyTrendPage.vue";
+import HomePortfolioPage from "./HomePortfolioPage.vue";
+import HomeRecommendStockPage from "./HomeRecommendStockPage.vue";
+</script>
 
-<style scoped></style>
+<style scoped>
+.home {
+  padding: 20px;
+}
+
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: 1px solid #ccc;
+}
+
+.summary,
+.portfolio,
+.recommendations {
+  margin-top: 20px;
+}
+
+.report-time {
+  color: #888;
+  font-size: 14px;
+}
+
+.portfolio,
+.recommendations {
+  border-top: 1px solid #ccc;
+  padding-top: 15px;
+}
+</style>
