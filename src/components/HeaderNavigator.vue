2860417ccc4fe24f48c1bf07bdd375f00e3e74a0
heebp <skdnfl4343@gmail.com>
2024-10-09 16:53:47 +0900

    refacotr: 종목에 따라 헤더 변경하는 기능 구현

--- src/components/HeaderNavigator.vue
+++ src/components/HeaderNavigator.vue
@@ -4,7 +4,16 @@
     <button v-if="showBackButton" class="back-button" @click="goBack">
       <img src="@/assets/icons/backButton-icon.svg" alt="Back" />
     </button>
-    <p class="header-title">{{ currentTitle }}</p>
+    <div v-if="stockName != '' || shortCode != ''" class="header-stock-section">
+      <div class="stockRow">
+        <div class="stockName">
+          {{ stockName }}
+        </div>
+        <div class="stockShortCode">
+          {{ shortCode }}
+        </div>
+      </div>
+    </div>
     <div class="header-actions"></div>
   </header>
 </template>
@@ -23,7 +32,8 @@ const showBackButton = computed(() => {
   return router.currentRoute.value.name !== "home";
 });
 
-const currentTitle = computed(() => headerStore.title);
+const stockName = computed(() => headerStore.stockName);
+const shortCode = computed(() => headerStore.shortCode);
 
 const goBack = () => router.back();
 </script>
@@ -36,6 +46,7 @@ p {
   display: flex;
   flex-direction: row;
   align-items: center;
+  z-index: 1000;
   padding: 12px 8px 12px 16px;
   gap: 8px;
   isolation: isolate;
@@ -62,7 +73,25 @@ p {
   transform: translateX(-50%); /* 가로 가운데 정렬 */
   padding-bottom: 0; /* 아래 패딩 제거 */
 }
-
+.header-stock-section {
+  font-family: "Pretendard-Bold", sans-serif;
+  position: absolute;
+  left: 50%;
+  transform: translateX(-50%); /* 가로 가운데 정렬 */
+  display: flex;
+  flex-direction: row;
+  padding-bottom: 0; /* 아래 패딩 제거 */
+}
+.stockName {
+  font-size: 16px;
+  padding-top: 10px;
+  line-height: 15px;
+}
+.stockShortCode {
+  text-align: center;
+  color: #9ea6b8;
+  font-size: 12px;
+}
 .header-actions {
   flex: none;
   order: 1;
