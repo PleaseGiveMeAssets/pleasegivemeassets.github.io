57da1d281d410ba417e4dc2773f683e1fa66fbcd
Hyejeng443 <buty1013@naver.com>
2024-10-15 17:07:58 +0900

    feat : 설문 로딩페이지 따로 구현, 동전 이미지 변경,  닉네임 로컬스토리지에 저장

--- src/components/LoadingComponent.vue
+++ src/components/LoadingComponent.vue
@@ -9,7 +9,7 @@
       />
       <!-- 회전하는 동전 아이콘 -->
       <img
-        src="@/assets/icons/coin-icon.svg"
+        src="@/assets/icons/coin-icon-kb.svg"
         alt="loading-icon"
         class="rotating-icon"
       />
@@ -33,13 +33,21 @@
 </template>
 
 <style scoped>
+html,
+body {
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 100%;
+}
+
 .loading-overlay {
   position: fixed;
   top: 0;
   left: 0;
   width: 100%;
   height: 100%;
-  background-color: #ffffff; /* 배경을 흰색으로 설정 */
+
   display: flex;
   justify-content: center;
   align-items: center;
@@ -58,11 +66,11 @@
 }
 
 .rotating-icon {
-  width: 60px;
-  height: 60px;
+  width: 100px;
+  height: 100px;
   position: absolute;
-  top: 38%; /* 너구리 아이콘의 중간 지점 */
-  left: 100px; /* 너구리 아이콘과 간격 조정 */
+  top: -3%; /* 너구리 아이콘의 중간 지점 */
+  left: 92px; /* 너구리 아이콘과 간격 조정 */
   transform: translateY(-50%);
   animation: spin 1s linear infinite; /* 빙글빙글 도는 애니메이션 */
 }
