90aea82a20cc0ab29a8816f9338642a3c80def2f
Park Hyewon <db1648@naver.com>
2024-10-15 16:59:25 +0900

    refacter: firebase 연결 및 dailyReport ui수정

--- src/pages/DailyReportPage.vue
+++ src/pages/DailyReportPage.vue
@@ -1,34 +1,29 @@
 <template>
-  <div class="daily-trend">
-    <div v-if="isLoading">
-      <LoadingComponent />
+  <div>
+    <div class="logo-section">
+      <img class="logo-img" src="@/assets/images/logo-gray.png" />
     </div>
-    <div v-show="!isLoading">
-      <TrendStock @loaded="onComponentLoaded" />
-      <RecommendStock @loaded="onComponentLoaded" />
-    </div>
+    <TrendStock />
+    <div class="gap-div"></div>
+    <RecommendStock />
   </div>
 </template>
 
 <script setup>
 import TrendStock from "../components/dailyreport/TrendStock.vue";
 import RecommendStock from "../components/dailyreport/RecommendStock.vue";
-import { ref } from "vue";
-import LoadingComponent from "@/components/LoadingComponent.vue";
-
-const isLoading = ref(true);
-const loadingCount = ref(2);
-
-const onComponentLoaded = () => {
-  loadingCount.value -= 1;
-  if (loadingCount.value === 0) {
-    isLoading.value = false;
-  }
-};
 </script>
 
 <style scoped>
-.daily-trend {
-  padding: 20px;
+.logo-section {
+  padding: 20px 0 20px 0;
+}
+
+.logo-section > img {
+  width: 60px;
+}
+
+.gap-div {
+  height: 32px;
 }
 </style>
